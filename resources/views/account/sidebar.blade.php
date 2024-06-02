b0feae4e7f8ce8c66bd04cb069a293fe1b1e3bc8
Janos Stelli <stellijanos23@gmail.com>
2024-06-02 14:37:45 +0300

    create controller and implement account home page

---
+++ resources/views/account/sidebar.blade.php
@@ -0,0 +1,16 @@
+<div class="col-md-3">
+    <div class="card">
+        <div class="card-header">
+            My Account
+        </div>
+        <div class="card-body">
+            <a href="" class="nav-link">Favourites</a> 
+            <a href="" class="nav-link">Orders</a> 
+            <a href="" class="nav-link">Saved Addresses</a> 
+            <a href="" class="nav-link">Update Personal Info</a> 
+            <a href="" class="nav-link">Change password</a> 
+            <a href="" class="nav-link">Delete Account</a> 
+            <a href="" class="nav-link">Logout</a>
+        </div>
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+
+Route::resource('/account', UserController::class);
+
