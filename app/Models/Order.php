affc083ef88a7551f8635dcf2880c0171d1f2662
Janos Stelli <stellijanos23@gmail.com>
2024-06-05 08:55:31 +0300

    implement realtionships between models

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,8 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Order extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'address_id',
+        'phone',
+        'status',
+        'payment',
+        'shipping_fee',
+    ];
+
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
+    
+    public function address(): BelongsTo {
+        return $this->belongsTo(Address::class);
+    }
+
+    public function orderItems(): HasMany {
+        return $this->hasMany(OrderItem::class);
+    }
+
 }
