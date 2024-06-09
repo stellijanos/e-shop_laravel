e8c8a1327350621510c10a71cf87cf782963b129
Janos Stelli <stellijanos23@gmail.com>
2024-06-09 16:27:12 +0300

    improve navbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,10 +7,11 @@
             <div class="bg-body-secondary text-light-emphasis rounded text-center shadow pt-3" style="width:150px">
                 <img class="rounded" src="{{asset('public/images/products/'.$product->image)}}" style="width:120px; height:120px" alt="{{$product->name}}-image">
                 <figcaption>
-                    <p class="text-truncate fs-3 fw-bold" style="max-width: 150px;">{{$product->name}}</p>
-                    <p class="text-truncate" style="max-width: 150px;">{{$product->description}}</p>
-                    <p class="fs-5 text-start fw-bold">${{$product->price}}</p>
-                    <div class="row justify-content-around mb-3">
+                    <p class="text-truncate fs-3 fw-bold m-0" style="max-width: 150px;">{{$product->name}}</p>
+                    <p class="text-truncate fw-bold m-0" style="max-width: 150px;">({{$product->category->name}})</p>
+                    <p class="text-truncate m-1" style="max-width: 150px;">{{$product->description}}</p>
+                    <p class="fs-5 text-start fw-bold m-0">${{$product->price}}</p>
+                    <div class="row justify-content-around mb-1">
                         <button class="btn btn-white col-4"><i class="fa-solid fa-heart fa-2x" style="color:red; height:"></i></button>
                         <button class="btn btn-white col-4"><i class="fa-solid fa-cart-plus fa-2x"></i></button>
                     </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,6 +17,27 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" crossorigin="anonymous">
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
+    <style>
+        a.dropdown-item:hover {
+            background-color:lightgray;
+        }
+
+        li.nav-item {
+            border-radius:10px;
+        }
+
+        li.nav-item:hover {
+            background-color: lightgray;
+        }
+
+        footer {
+            position: absolute;
+            bottom: 0;
+            width: 100%;
+            height: 2.5rem;
+        }
+    </style>
 </head>
 <body>
     <div id="app">
@@ -24,7 +45,8 @@
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
-                </a>
+                    </a>
+                <div class="vr" style="margin-right:10px"></div>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -32,7 +54,10 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-
+                        <div class="input-group">
+                            <input type="search" class="form-control" style="width:25vw;" name="search_string" id="search-string" placeholder="Search items...">
+                            <button class="input-group-text"><i class="fa-solid fa-magnifying-glass"></i></button>
+                        </div>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
@@ -53,20 +78,32 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->firstname }}
+                                 <i class="fa-solid fa-user"></i> {{ Auth::user()->firstname }}
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+
+                                    @if(Auth::user()->role === 'customer')
+
+                                    @endif
+
+                                    <a href="{{url('/account')}}" class="dropdown-item">My Account</a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
-
+                                    
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
                                     </form>
                                 </div>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href=""><i class="fa-solid fa-heart"></i> Favourites</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href=""><i class="fa-solid fa-cart-shopping"></i> Shopping Cart</a>
                             </li>
                         @endguest
                     </ul>
@@ -78,5 +115,6 @@
             @yield('content')
         </main>
     </div>
+    <footer class="text-center mb-0">&copy; Stelli Janos. All rights reserved.</footer>
 </body>
 </html>
