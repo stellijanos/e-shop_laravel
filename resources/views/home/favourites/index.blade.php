d0a9c137b1dd7c5b30d2126a28af1e634dc54bf2
Janos Stelli <stellijanos23@gmail.com>
2024-06-17 15:59:19 +0300

    implement no products were found

--- resources/views/home/favourites/index.blade.php
+++ resources/views/home/favourites/index.blade.php
@@ -19,37 +19,44 @@
     }
 
 </style>
-    
-<div class="container align-self-start">
-    <form class="row mb-3">
-        <div class="col-3">
-            <div class=" input-group">
-                <label class="input-group-text" for="sort-by">Sort by: </label>
-                <select name="sort_by" id="sort-by" class="form-select" onchange="this.form.submit()">
-                    <option value="_">Default</option>
-                    <option value="price-asc" {{$sort_by=== "price-asc" ? 'selected' : ''}}>Price (Ascending)</option>
-                    <option value="price-desc" {{$sort_by === "price-desc" ? 'selected' : ''}}>Price (Descending)</option>
-                </select>
-            </div>
-        </div>
-        <div class="col-4">
-            <div class=" input-group" style="width:200px;">
-                <label class="input-group-text" for="show-per-page">Show per page: </label>
-                <select class="form-select" name="per_page" id="show-per-page" onchange="this.form.submit()">
-                    @foreach ($per_page_values as $page_value)
-                        <option value="{{$page_value}}" {{$per_page === $page_value ? 'selected' : ''}}>{{$page_value}}</option>
-                    @endforeach
-                </select>
-            </div>
-        </div>
-    </form>
 
-    <div class="d-flex flex-row justify-content-start flex-wrap gap-5 mb-5" id="product-list">
-        @include('home.favourites.products')
-    </div>
-    <div class="text-center">
-            {{$products->links()}}
-    </div>
+<div class="container align-self-start">
+
+    @if($products->count() === 0 )
+        @include('home.no-products')
+    @else   
+        <form class="row mb-3">
+            <div class="col-3">
+                <div class=" input-group">
+                    <label class="input-group-text" for="sort-by">Sort by: </label>
+                    <select name="sort_by" id="sort-by" class="form-select" onchange="this.form.submit()">
+                        <option value="_">Default</option>
+                        <option value="price-asc" {{$sort_by=== "price-asc" ? 'selected' : ''}}>Price (Ascending)</option>
+                        <option value="price-desc" {{$sort_by === "price-desc" ? 'selected' : ''}}>Price (Descending)</option>
+                    </select>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class=" input-group" style="width:200px;">
+                    <label class="input-group-text" for="show-per-page">Show per page: </label>
+                    <select class="form-select" name="per_page" id="show-per-page" onchange="this.form.submit()">
+                        @foreach ($per_page_values as $page_value)
+                            <option value="{{$page_value}}" {{$per_page === $page_value ? 'selected' : ''}}>{{$page_value}}</option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
+        </form>
+        <div class="d-flex flex-row justify-content-start flex-wrap gap-5 mb-5" id="product-list">
+            @include('home.favourites.products')
+        </div>
+        <div class="text-center">
+                {{$products->links()}}
+        </div>    
+
+    @endif
 </div>
+
+
 @include('home.product-handling')
 @endsection
---
+++ resources/views/home/no-products.blade.php
@@ -0,0 +1,4 @@
+
+<div id="no-products" class="text-center" style="width:100%; height:500px; display:flex; justify-content:center; align-items:center">
+    <p class="fs-1 fw-bold">No products were found.</p>
+</div>
