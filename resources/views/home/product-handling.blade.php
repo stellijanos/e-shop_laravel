7dd852eee6b15ad7ed9f64d6b3f3a4578cfe0302
Janos Stelli <stellijanos23@gmail.com>
2024-06-17 08:15:28 +0300

    implement favourite page reloading

--- resources/views/home/product-handling.blade.php
+++ resources/views/home/product-handling.blade.php
@@ -2,23 +2,38 @@
 
 <script>
 
+
+
+
+
+
     function favourite(button) {
 
-        const productId = button.getAttribute('data-product-id');
+        return new Promise((resolve, reject)=> {
+            const productId = button.getAttribute('data-product-id');
 
-        axios.post(`{{url('/account/favourite')}}/${productId}`)
-        .then(response => {
-            let responseText = response.data.response;
-            if (responseText === "added") {
-                button.innerHTML = '<i class="fa-solid fa-heart fa-2x" style="color:red;"></i>';
-            } else if (responseText === "removed") {
-                button.innerHTML = '<i class="fa-regular fa-heart fa-2x" ></i>'
-            }
-        })
-        .catch(error => {
-            //
+            axios.post(`{{url('/account/favourite')}}/${productId}`)
+            .then(response => {
+                reloadFavourites();
+                let responseText = response.data.response;
+                if (responseText === "added") {
+                    button.innerHTML = '<i class="fa-solid fa-heart fa-2x" style="color:red;"></i>';
+                } else if (responseText === "removed") {
+                    button.innerHTML = '<i class="fa-regular fa-heart fa-2x" ></i>'
+                }
+            })
+            .catch(error => {
+                //
+            });
         });
     }
+
+    function reloadFavourites() {
+        if (window.location.href.includes('/favourites')) {
+            window.location.reload();
+        }
+    }
+
 
     function addToCart(button) {
         const productId = button.getAttribute('data-product-id');
