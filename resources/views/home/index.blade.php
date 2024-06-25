e6617433a6ead3f54728ef560dc03b7b24357100
Janos Stelli <stellijanos23@gmail.com>
2024-06-25 09:08:38 +0300

    simplify no product handling

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,41 +1,40 @@
 @extends('layouts.app')
 @section('content')
 @include('product.css')
-<form class="d-flex flex-direction-row" action="{{route('home')}}" method="get">
-    <div class="col-2 accordion" style="padding:0 30px 0 10px; width:250px; " id="accordionPanelsStayOpenExample" id="filters">
-        @foreach ($filters as $filter => $values)
-            @php 
-                $index_1 = $loop->index;
-            @endphp
-            <div class="accordion-item">
-                <h2 class="accordion-header">
-                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapse-{{$index_1}}" aria-expanded="true" aria-controls="panelsStayOpen-collapse-{{$index_1}}">
-                        {{ucfirst($filter)}}
-                    </button>
-                </h2>
-                <div id="panelsStayOpen-collapse-{{$index_1}}" class="accordion-collapse collapse show">
-                    <div class="accordion-body">
-                        @foreach($values as $value => $checked) 
-                            <div>
-                                <input type="checkbox" class="mx-2 form-check-input" name="{{$filter}}[]" id="{{$value}}-{{$index_1}}-{{$loop->index}}" value="{{$value}}" {{$checked}} onchange="this.form.submit()">
-                                <label for="{{$value}}-{{$index_1}}-{{$loop->index}}">{{$value}}</label>
-                            </div>
-                        @endforeach
+
+@if($products->count() !== 0)
+    <form class="d-flex flex-direction-row" action="{{route('home')}}" method="get">
+        <div class="col-2 accordion" style="padding:0 30px 0 10px; width:250px; " id="accordionPanelsStayOpenExample" id="filters">
+            @foreach ($filters as $filter => $values)
+                @php 
+                    $index_1 = $loop->index;
+                @endphp
+                <div class="accordion-item">
+                    <h2 class="accordion-header">
+                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapse-{{$index_1}}" aria-expanded="true" aria-controls="panelsStayOpen-collapse-{{$index_1}}">
+                            {{ucfirst($filter)}}
+                        </button>
+                    </h2>
+                    <div id="panelsStayOpen-collapse-{{$index_1}}" class="accordion-collapse collapse show">
+                        <div class="accordion-body">
+                            @foreach($values as $value => $checked) 
+                                <div>
+                                    <input type="checkbox" class="mx-2 form-check-input" name="{{$filter}}[]" id="{{$value}}-{{$index_1}}-{{$loop->index}}" value="{{$value}}" {{$checked}} onchange="this.form.submit()">
+                                    <label for="{{$value}}-{{$index_1}}-{{$loop->index}}">{{$value}}</label>
+                                </div>
+                            @endforeach
+                        </div>
                     </div>
                 </div>
-            </div>
-        @endforeach
-    </div>
-    <div class="col-10 row align-self-start" id="product-list">
-        @if($products->count() === 0)
-            <div style="margin-left:-100px">
-                @include('home.no-products')
-            </div>
-        @else
+            @endforeach
+        </div>
+        <div class="col-10 row align-self-start" id="product-list">
             @include('home.products')
-        @endif 
-    </div>
-</form>
+        </div>
+    </form>
+@else
+    @include('home.no-products')
+@endif 
 
 @auth
     @include('product.js-handling')
